a7f6444c5235269e12909bed58a95ceeec1920ca
Tobi <tobi.bechtold@gmail.com>
2019-08-06 20:55:03 +0200

    initial revision with support for maclife rss feed

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var feed = require('rss-to-json');
+var app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+app.get('/', (req, res) => {
+    feed.load('http://www.maclife.de/rss/news.xml', (err, rss) => {
+        var feedItems = rss;
+        res.render('index', {feedItems: feedItems.items});
+    });
+    
+});
+
+app.listen(3000, () => {
+    console.log('express-rss-reader is running on port 3000.');
+});
